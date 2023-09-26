e8a79c76e3b90e8b716018960a4d04c8824063ea
Michael-byte-cell <144836683+Michael-byte-cell@users.noreply.github.com>
2023-09-26 13:59:17 -0400

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -1,2 +1,11 @@
-bill = input("Please enter your bill")
-print(float(bill))
+number = input("Please enter in a number:")
+if number % 2 == 0:
+    print("even")
+elif number % 2 == 1:
+    print("odd")
+
+
+
+
+
+
