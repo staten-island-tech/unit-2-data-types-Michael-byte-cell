919d20ff7de27732833dbd294bf45656c574aa4f
Michael-byte-cell <144836683+Michael-byte-cell@users.noreply.github.com>
2023-09-28 14:01:00 -0400

    Update stuff2.py

--- stuff2.py
+++ stuff2.py
@@ -1,20 +1,9 @@
-# Let there be two variables, Westbound and Eastbound. 
-# One variable (one or the other) can only be active at all times. 
-# If Westbound is active (or true), eastbound cannot be active (or false)
-# Because the flow of traffic only goes one way for 5 minutes at a time, we switch when we reach that point
-# something similar would be if westbound traffic = 5 then
-# westbound traffic would be false and eastbound traffic would be true, same applies for vice versa
+x = False
+y = True
 
-east_bound_traffic = 0
-west_bound_traffic = 1
-if east_bound_traffic == True and west_bound_traffic == True:
-    print("False")
-
-if east_bound_traffic == True and west_bound_traffic == False:
-    print("True")
-
-if east_bound_traffic == False and west_bound_traffic == True:
-    print("True")
-
-if east_bound_traffic == False and west_bound_traffic == False:
-    print("True")
+def truthy(x,y):
+    if x == y:
+        print("False")
+    elif x != y:
+        print("True")
+truthy(x,y)
